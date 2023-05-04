81f847d4f40166d967ba305f8da87dbb3033d456
mikehac <mike.hatskevich@gmail.com>
2023-05-04 14:39:12 +0300

    adding validation on client side

--- CodeOasis.Front/src/app/app.component.ts
+++ CodeOasis.Front/src/app/app.component.ts
@@ -10,17 +10,34 @@ export class AppComponent {
   moves: string = '';
   countDownInscrutions: number = 15;
   movesList: string[][] = [];
+  showValidationError: boolean = false;
 
   constructor(private robotService: RobotService) {}
 
   public send() {
-    if (this.moves != '') {
-      this.countDownInscrutions--;
-      this.robotService
-        .trackRobot(JSON.parse(this.moves))
-        .subscribe((location) => {
-          this.movesList.push(location);
-        });
+    if (this.moves == '' || !this.isValidJSON(this.moves)) {
+      this.showValidationError = true;
+      return;
     }
+
+    this.showValidationError = false;
+    this.countDownInscrutions--;
+    this.robotService.trackRobot(JSON.parse(this.moves)).subscribe(
+      (location) => {
+        this.movesList.push(location);
+        this.moves = '';
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+  private isValidJSON(moves: string): boolean {
+    try {
+      JSON.parse(moves);
+    } catch (e) {
+      return false;
+    }
+    return true;
   }
 }
