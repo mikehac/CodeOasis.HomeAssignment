e842ffbe2a63b512f9752f807987925803290a35
mikehac <mike.hatskevich@gmail.com>
2023-05-04 11:07:41 +0300

    Add out of boundaries validation

--- CodeOasis.HomeAssignment/Service/RobotService.cs
+++ CodeOasis.HomeAssignment/Service/RobotService.cs
@@ -2,10 +2,12 @@
 {
     public class RobotService : IRobotService
     {
+        const int MAX_HORIZONTAL = 50;
+        const int MAX_VERTICAL = 50;
         public int[] TrackRobot(string[] movements)
         {
             if (!IsValidInput(movements))
-                throw new ArgumentException("The input is invalid");
+                throw new ArgumentException("The input is invalid");            
 
             Dictionary<string, int> parsedMovement = new Dictionary<string, int>();
             foreach (var movement in movements)
@@ -27,7 +29,20 @@
 
             int horizontal = right - left;
             int vertical = up - down;
+            if (OutOfBoundaries(horizontal,vertical))
+                throw new ApplicationException("The robot can't go out of boundaries");
+
             return new int[] { horizontal, vertical };
+        }
+
+        private bool OutOfBoundaries(int horizontal, int vertical)
+        {
+            if (horizontal > MAX_HORIZONTAL ||
+                horizontal < -MAX_HORIZONTAL ||
+                vertical > MAX_VERTICAL ||
+                vertical < -MAX_VERTICAL)
+                return true;
+            return false;
         }
 
         private bool IsValidInput(string[] movements)
